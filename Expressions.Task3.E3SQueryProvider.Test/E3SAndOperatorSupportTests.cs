f939e7552ce591094e42362ef31c648aad0984ce
Aydin Valiyev <aydin0294@gmail.com>
2023-05-28 22:26:14 +0200

    first commit

---
+++ Expressions.Task3.E3SQueryProvider.Test/E3SAndOperatorSupportTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+using System.Linq.Expressions;
+using Expressions.Task3.E3SQueryProvider.Models.Entities;
+using Xunit;
+
+namespace Expressions.Task3.E3SQueryProvider.Test
+{
+    public class E3SAndOperatorSupportTests
+    {
+        #region SubTask 3: AND operator support
+
+        [Fact]
+        public void TestAndQueryable()
+        {
+            var translator = new ExpressionToFtsRequestTranslator();
+            Expression<Func<IQueryable<EmployeeEntity>, IQueryable<EmployeeEntity>>> expression
+                = query => query.Where(e => e.Workstation == "EPRUIZHW006" && e.Manager.StartsWith("John"));
+
+            var translated = translator.Translate(expression);
+
+            Assert.Equal("Workstation:(EPRUIZHW006) AND Manager:(John*)", translated);
+        }
+
+        #endregion
+    }
+}
